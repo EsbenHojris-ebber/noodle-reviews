90f0a786ba6456e790c66f4ea841924089114d43
Esben Højris <esbenhojris@gmail.com>
2023-01-18 14:39:17 +0100

    Specify better data annotations

--- NoodleReviews/Models/NoodlePack.cs
+++ NoodleReviews/Models/NoodlePack.cs
@@ -6,18 +6,31 @@ namespace NoodleReviews.Models;
 public class NoodlePack
 {
     public int Id { get; set; }
+
     [Display(Name = "Navn")]
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public required string Name { get; set; }
+
     [Display(Name = "Stregkode")]
+    [RegularExpression(@"^[0-9]+$")]
+    [Required]
     public required string BarCode { get; set; }
+
     [Display(Name = "Noter")]
+    [StringLength(512)]
     public string? Notes { get; set; }
+
     [Display(Name = "Anmeldelsesdato")]
     [DataType(DataType.Date)]
     public DateTime ReviewDate { get; set; }
+
     [Display(Name = "Karakter")]
     public Grade Grade { get; set; }
+
     [Display(Name = "Pris")]
+    [Range(0, 300)]
+    // [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(9, 2)")]
     public decimal Price { get; set; }
 }
