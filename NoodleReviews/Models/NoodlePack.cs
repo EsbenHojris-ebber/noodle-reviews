b86be19077528f4043fea8a2a4127e817ef36902
Esben Højris <esbenhojris@gmail.com>
2023-01-18 12:54:15 +0100

    Annotated Noodlepack model to change display

--- NoodleReviews/Models/NoodlePack.cs
+++ NoodleReviews/Models/NoodlePack.cs
@@ -1,15 +1,21 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NoodleReviews.Models;
 
 public class NoodlePack
 {
     public int Id { get; set; }
+    [Display(Name = "Navn")]
     public required string Name { get; set; }
+    [Display(Name = "Stregkode")]
     public required string BarCode { get; set; }
+    [Display(Name = "Noter")]
     public string? Notes { get; set; }
+    [Display(Name = "Anmeldelsesdato")]
     [DataType(DataType.Date)]
     public DateTime ReviewDate { get; set; }
+    [Display(Name = "Karakter")]
     public Grade Grade { get; set; }
 }
 
